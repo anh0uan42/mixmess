c6c28002f1ae9538dd88c918763546f6e6b83891
Q Nguyen <nguyenanhquancttpcl@gmail.com>
2025-07-29 21:06:01 -0500

    add a js file

---
+++ main.js
@@ -0,0 +1,21 @@
+const noun1 = ['The sea', 'The wind', "The suns"];
+const sunVerb = ['is too hot', 'is perfect'];
+const windVerb = ['is too strong', 'is not blowing', 'is at a perfect speed'];
+const seaVerb = ['too rough', 'is about 2-3ft', 'is glass calm'];
+const act = ['fishing', 'surfing']
+
+const generateMessage = (arr1, arr2, arr3, arr4, arr5) => {
+    const noun = arr1[Math.floor(Math.random()*(arr1.length))]
+    let verb = ''
+    if (noun === 'The suns') {
+        verb = arr2[Math.floor(Math.random()*arr2.length)]
+    } else if (noun === 'The wind') {
+        verb = arr3[Math.floor(Math.random()*arr3.length)]
+    } else {
+        verb = arr4[Math.floor(Math.random()*arr4.length)]
+    }
+    const action = arr5[Math.floor(Math.random()*arr5.length)]
+    console.log(`${noun} ${verb} to go ${action} this weekend`)
+}
+
+generateMessage(noun1, sunVerb, windVerb, seaVerb, act)
